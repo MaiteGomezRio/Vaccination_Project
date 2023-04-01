d40fecd38255c66858bc4e0c3f6dfcf2da6fd663
carmengarciaprieto <127328091+carmengarciaprieto@users.noreply.github.com>
2023-04-01 14:12:05 +0200

    Add files via upload

---
+++ Patient.java
@@ -0,0 +1,93 @@
+package vaccination.pojos; 
+
+import java.util.List;
+import java.util.Objects; 
+
+public class Patient {
+    
+	private Integer id; 
+	private String name; 
+	private String surname; 
+	private boolean attendance; 
+	private boolean immune; 
+	private List<Vaccine> vaccines;
+	private Doctor doctor; 
+	
+	//nose como enlazar con disease y con condition 
+	
+	public Patient () {
+		super(); 
+		vaccines = new ArrayList<Vaccine>; 
+	}
+	public Patient(Integer id, String name, String surname, Boolean attendance, Boolean immumne) {
+		this.id = id; 
+		this.name = name; 
+		this.surname = surname;
+		this.attendance = attendance; 
+		this.immune = immune; 
+		vaccines = new ArrayList<Vaccine>();  //ALWAYS INITIALIZE THE LISTS  
+	}
+	@Override 
+	public String toString() {
+		return "Patient [id: "+id+", name: "+name+" ,surname: "+surname+" attendance: "+attendance+", immune: "+immune+ "]";
+	}
+	
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getSurname() {
+		return surname;
+	}
+	public void setSurname(String surname) {
+		this.surname = surname;
+	}
+	public boolean isAttendance() {
+		return attendance;
+	}
+	public void setAttendance(boolean attendance) {
+		this.attendance = attendance;
+	}
+	public boolean isImmune() {
+		return immune;
+	}
+	public void setImmune(boolean immune) {
+		this.immune = immune;
+	}
+	public List<Vaccine> getVaccines() {
+		return vaccines;
+	}
+	public void setVaccines(List<Vaccine> vaccines) {
+		this.vaccines = vaccines;
+	}
+	public Doctor getDoctor() {
+		return doctor;
+	}
+	public void setDoctor(Doctor doctor) {
+		this.doctor = doctor;
+	}
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Patient other = (Patient) obj;
+		return Objects.equals(id, other.id);
+	}
+	
+}
