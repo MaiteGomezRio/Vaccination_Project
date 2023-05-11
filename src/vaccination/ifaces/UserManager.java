e4e10127b51515f253bcf4e87de7799c52199fe5
carmengarciaprieto_ <carmengarciaprieto_@localhost>
2023-05-11 18:25:21 +0200

    Setted up jpa and created user and role

---
+++ src/vaccination/ifaces/UserManager.java
@@ -0,0 +1,15 @@
+package vaccination.ifaces;
+
+import java.util.List;
+
+import vaccination.pojos.*; 
+
+public interface UserManager {
+	  
+      public void register(User user);   
+      public void assignRole(User user, Role role);
+      //public void assignRole(int userId, int roleId);  //another way of doing it
+      public User login(String name, String password);   //if user doesnt exit return null 
+      public void createRole(Role role); 
+      public List<Role> getRoles(); 
+}
