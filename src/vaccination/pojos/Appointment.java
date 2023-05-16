4a606de04cd64e080c2235a5d140da2c6fc291d8
carmengarciaprieto_ <carmengarciaprieto_@localhost>
2023-05-16 20:00:41 +0200

    setAppointment

--- src/vaccination/pojos/Appointment.java
+++ src/vaccination/pojos/Appointment.java
@@ -1,6 +1,7 @@
 package vaccination.pojos;
 
 import java.io.Serializable;
+import java.sql.Date;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
@@ -9,35 +10,41 @@ public class Appointment implements Serializable{
 	
 	private static final long serialVersionUID = -8007500555158752133L;
 	private int id;
-	private LocalDate date;
+	private Date date;
 	private Doctor doctor;
-	private Patient patients; 
+	private Patient patient; 
 	private Vaccine vaccine; 
 
 	
 	public Appointment() {
 		super(); 
 	}
-	public Appointment(LocalDate date) {
+	public Appointment(Date date) {
 		super(); 
 		this.date = date; 
 	}
-	
+	//TODO EL DOCTOR SE ASIGNA AUTOMATICAMENTE??
+	public Appointment(Date date, Patient patient, Vaccine vaccine) {
+		super();
+		this.date = date;
+		this.patient = patient;
+		this.vaccine = vaccine;
+	}
 	public int getId() {
 		return id;
 	}
 	public void setId(int id) {
 		this.id = id;
 	}
-	public LocalDate getDate() {
+	public Date getDate() {
 		return date;
 	}
-	public void setDate(LocalDate date) {
+	public void setDate(Date date) {
 		this.date = date;
 	}
 	@Override
 	public String toString() {
-		return "Puts [id=" + id + ", date=" + date + ", doctor=" + doctor + ", patients=" + patients + ", vaccine="
+		return "Puts [id=" + id + ", date=" + date + ", doctor=" + doctor + ", patients=" + patient + ", vaccine="
 				+ vaccine + "]";
 	}
     
