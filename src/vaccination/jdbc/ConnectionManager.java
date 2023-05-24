4fd84e350a565197329cdfd51edf559d5e2f686f
MaiteGomezRio <maite.gomezriovinuesa@usp.ceu.es>
2023-05-24 23:14:29 +0200

    connection manager

--- src/vaccination/jdbc/ConnectionManager.java
+++ src/vaccination/jdbc/ConnectionManager.java
@@ -2,8 +2,12 @@ package vaccination.jdbc;
 
 import java.sql.Connection; 
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+
+import vaccination.pojos.Condition;
 
 
 public class ConnectionManager {
@@ -17,6 +21,7 @@ public class ConnectionManager {
 			c.createStatement().execute("PRAGMA foreign_keys=ON");
 			System.out.println("Database connection opened.");
 			createTables();
+			insertTables();
 		} catch (Exception e) {
 			System.out.println("Database access error");
 			e.printStackTrace();
@@ -70,7 +75,7 @@ public class ConnectionManager {
 
 			s.close();
 		} catch (SQLException e) {
-			// Check if the exception is because the tables already exist
+			
 			if (e.getMessage().contains("already exist")) {
 				return;
 			}
@@ -90,4 +95,155 @@ public class ConnectionManager {
 		}
 	}
 	
+	
+		
+	public void insertTables() { 
+		try {
+		Statement s = c.createStatement(); 
+		String insert_Disease= "INSERT INTO Disease (name)VALUES ('Haemophilus'),"
+				+ "('Pneumococcal'), ('SRP'),('Measles'),('Papilloma(HPV)'),('Poliomyelitis'),('Diphteria'),('HepatitisB'),"
+				+ "('Dtpa'),('Meningococcus'),('Covid-19')"; 
+		s.execute(insert_Disease);
+		
+		String insert_Condition="INSERT INTO Condition(name)VALUES ('Pregnant'),('Allergies'),('HIV'),('Stroke')"
+				+ "('Ictus'),('Epilepsy'),('Pneumonia'),('Special medical condition')";
+		s.execute(insert_Condition);
+		
+		s.close(); 
+		}catch(SQLException e) {
+		System.out.println("database error");
+		e.printStackTrace();
+		}
+	}
+	
+	public void insertVaccines() {
+		int id;
+		try {
+		String select_Disease="SELECT id FROM Disease WHERE name=?";
+		String insert_Vaccine="INSERT INTO Vaccine(name, dose, disease_id) VALUES (?,?,?)";
+		PreparedStatement p = c.prepareStatement(select_Disease); 
+		PreparedStatement p2=c.prepareStatement(insert_Vaccine);
+		p.setString(1, "Haemophilus"); 
+		ResultSet rs = p.executeQuery();
+		rs.next();
+		id=rs.getInt("id");
+		p2.setString(1, "Pfizer");
+		p2.setInt(2, 3);
+		p2.setInt(3,id);
+		
+		p.setString(1, "Pneumococcal"); 
+		rs=p.executeQuery();
+		rs.next();
+		id=rs.getInt("id");
+		
+		p.setString(1, "SRP"); 
+		rs=p.executeQuery();
+		rs.next();
+		id=rs.getInt("id");
+		
+		p.setString(1, "Measles"); 
+		rs=p.executeQuery();
+		rs.next();
+		id=rs.getInt("id");
+		
+		p.setString(1, "Papilloma(HPV)"); 
+		rs=p.executeQuery();
+		rs.next();
+		id=rs.getInt("id");
+		
+		p.setString(1, "Poliomyelitis"); 
+		rs=p.executeQuery();
+		rs.next();
+		id=rs.getInt("id");
+		
+		p.setString(1, "Diphteria"); 
+		rs=p.executeQuery();
+		rs.next();
+		id=rs.getInt("id");
+		
+		p.setString(1, "HepatitisB"); 
+		rs=p.executeQuery();
+		rs.next();
+		id=rs.getInt("id");
+		
+		p.setString(1, "Dtpa"); 
+		rs=p.executeQuery();
+		rs.next();
+		id=rs.getInt("id");
+		
+		p.setString(1, "Meningococcus"); 
+		rs=p.executeQuery();
+		rs.next();
+		id=rs.getInt("id");
+		
+		
+		
+		p.setString(1, "Covid-19"); 
+		rs=p.executeQuery();
+		rs.next();
+		id=rs.getInt("id");
+		p2.setString(1, "Pfizer");
+		p2.setInt(2, 3);
+		p2.setInt(3,id);
+		
+		
+		
+		
+		}catch(SQLException e) {
+			System.out.println("database error");
+			e.printStackTrace();
+		}
+		
+	}
+	
+
+	/*public void getDiseasesFromTables() {
+		try {
+			String sql="SELECT id FROM Disease WHERE name=?";
+			PreparedStatement p = c.prepareStatement(sql); 
+			p.setString(1, ""); 
+			ResultSet rs = p.executeQuery();
+			rs.next();
+		    int c_id=rs.getInt("id");
+	        Condition condition=new Condition(c_id,type);
+	        rs.close();
+	        p.close();
+		}catch(SQLException e) {
+			System.out.println("database error");
+			e.printStackTrace();
+			}
+	}*/
+	public void assignDiseaseToVaccineConnection(int d_id,int v_id) {
+		try {
+		String sql = "INSERT into Disease_Vaccine(disease_id, vaccine_id) WHERE VALUES (?,?)"; // vaccine
+		PreparedStatement p = c.prepareStatement(sql);
+		p.setInt(1, d_id);
+		p.setInt(2, v_id);
+		p.executeUpdate();
+		p.close();
+		} catch (SQLException e) {
+		System.out.println("database error");
+		e.printStackTrace();
+		}
+	}
+	
+	public int associateDisease(String name) {
+		try { 
+		String sql = "SELECT * FROM Disease WHERE name LIKE ?"; // vaccine
+		PreparedStatement p = c.prepareStatement(sql);
+		p.setString(1, name);
+		ResultSet rs = p.executeQuery();
+		rs.next(); 
+		int disease_id = rs.getInt("id");
+		p.close();
+		return disease_id ;
+		
+		} catch (SQLException e) {
+		System.out.println("database error");
+			e.printStackTrace();
+			return -1;
+		}
+	}
+		
+
 }
